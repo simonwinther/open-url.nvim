a1774fee93b9c2246e4fdbab360344130ab461f2
Simon Winther <simonwa01@gmail.com>
2026-01-18 16:26:00 +0100

    fix: bug when opening urls

--- lua/open_url/init.lua
+++ lua/open_url/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local function system_open(url)
+local function open(url)
 	if url:find("^www%.") then
 		url = "https://" .. url
 	end
@@ -9,24 +9,30 @@ end
 
 function M.open_at_line()
 	local line = vim.api.nvim_get_current_line()
-	if not (line:find("https?://") or line:find("www%.")) then
+
+	-- Fast Path
+	if not (line:find("http") or line:find("www%.")) then
 		return
 	end
 
 	local urls = {}
+	-- %S+ matches sequences of non-whitespace characters
 	for chunk in line:gmatch("%S+") do
 		if chunk:find("^https?://") or chunk:find("^www%.") then
-			table.insert(urls, chunk:gsub("[%]%)}%.,;:''\"%s]+$", ""))
+			-- Strip trailing punctuation
+			local url = chunk:gsub("[%]%)}%.,;:''\"%s]+$", "")
+			table.insert(urls, url)
 		end
 	end
 
-	if #urls == 1 then
-		system_open(urls[1])
-	elseif #urls > 1 then
+	local count = #urls
+	if count == 1 then
+		open(urls[1])
+	elseif count > 1 then
 		local snacks = _G.Snacks or require("snacks")
 		snacks.picker.select(urls, { prompt = "Open URL" }, function(choice)
 			if choice then
-				system_open(choice)
+				open(choice)
 			end
 		end)
 	end
